aa463efab9d47aac83037db03990dede800a58eb
Code Sandbox <hello@codesandbox.io>
2023-07-29 22:02:01 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.2835d97f42d709ef11d5fdd1cd27d00a.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f628b25f44cba2e21411a9c315b97783",
+    "url": "https://letlive419.github.io/csb-j769j7/index.html"
+  },
+  {
+    "revision": "e38129e8005cdaea68a6",
+    "url": "https://letlive419.github.io/csb-j769j7/static/js/main.435d47ed.chunk.js"
+  },
+  {
+    "revision": "5e4fa1d4ed1f35f2feb4",
+    "url": "https://letlive419.github.io/csb-j769j7/static/js/runtime-main.512cab61.js"
+  }
+]);
